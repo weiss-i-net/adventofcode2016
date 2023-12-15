a16497f2aab97172ee3004ededabcb1139398ab7
Jannik Hiller <jannik.hiller@rwth-aachen.de>
2023-12-15 15:00:17 -0800

    first commit

---
+++ template.py
@@ -0,0 +1,20 @@
+from collections import Counter, defaultdict, deque
+import functools as func
+import itertools as it
+import math
+import more_itertools as mit
+import networkx as nx
+import numpy as np
+import operator as op
+import re
+import sympy
+import sys
+
+def p(v, desc=None):
+    print(f"{desc}: {v}" if desc else v)
+    return v
+
+data = sys.stdin.read().splitlines()
+
+for line in data:
+    pass
